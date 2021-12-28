1b15a3ef2e7f56bfc0da924c7ef13e4fb07e3f3f
macc001 <miguelcayocarvajal37@gmail.com>
2021-12-28 09:54:55 -0400

    concat cadena a paratir de un array

--- src/app/00-list/list.component.ts
+++ src/app/00-list/list.component.ts
@@ -6,15 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-  list: number[] = [1, 1, 3, 4, 2, 6, 8];
-  isOrdenArreglo = false;
-
   constructor() {
-    this.isOrdenArreglo = this.arregloIsOrden(this.list);
+    this.isOrdenArreglo = this.arregloIsOrden(this.listNumber);
+    this.text = this.concatText(this.listText);
+    this.objectArray = this.object(this.properties);
   }
 
   ngOnInit() {}
 
+  listNumber: number[] = [1, 1, 3, 4, 2, 6, 8];
+  isOrdenArreglo = false;
   // comprobar si un arreglo se encuentra ordenado usando una funcion personalizada
   arregloIsOrden(data: any[]) {
     if (data.length <= 1) {
@@ -27,5 +28,27 @@ export class ListComponent implements OnInit {
       }
     }
     return true;
+  }
+
+  listText: string[] = ['pdf', 'jpg', 'png', 'gif', 'tif', 'tiff', 'txt'];
+  text = '';
+  // Concatenar elementos de un array con un separador pero con el último diferente
+  concatText(data: any[]) {
+    return data.slice(0, -1).join(', ') + ' y ' + data.slice(-1);
+  }
+
+  properties: string[] = ['folder1', 'folder2', 'folder3'];
+  objectArray!: any;
+  // Crear objeto a partir de un array, colocar un valor al ultimo elemento de la lista
+  object(data: any[]): any {
+    const parametro = { phone: '72476675', name: 'folder' };
+    const result = {};
+
+    data.reduce((acc: any, func, idx, arr) => {
+      acc.property = func;
+      acc.value = idx + 1 == arr.length ? parametro : {};
+      return acc.value;
+    }, result);
+    return result;
   }
 }
